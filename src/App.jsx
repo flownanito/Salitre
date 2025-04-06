4a74b6d46a7b67ea9fe51f9b51f8497e098b7213
flownanito <165549271+flownanito@users.noreply.github.com>
2025-04-06 22:30:23 +0100

    fix the last version

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Shop from "./pages/shop/Shop";
+import Team from "./pages/team/Team";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />}> </Route>
         <Route path="/Home" element={<Home />}> </Route>
-        <Route path="/profile" element={<Profile />}> </Route>
-        <Route path="/shopin" element={<Shop />}> </Route>
-        <Route path="*" element={<Home />}> </Route>
+        <Route path="/Profile" element={<Profile />}> </Route>
+        <Route path="/Shop" element={<Shop />}> </Route>
+        <Route path="/Team" element={<Team />}> </Route>
       </Routes>
     </BrowserRouter>
   )
